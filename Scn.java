d4d421063c90ef319880feceb442510381335e85
nandhinikg <135498569+nandhinikg@users.noreply.github.com>
2023-06-05 07:30:09 +0400

    intial coding

---
+++ Scn.java
@@ -0,0 +1,20 @@
+package org.scn;
+import java.util.Scanner;
+public class Scn {
+	public static void main(String[] args) {
+
+	System.out.println("enter bank name");
+	
+	Scanner w=new Scanner (System.in);
+	String bankname = w.nextLine();
+	System.out.println("bankname="+bankname);
+	
+     int interest = w.nextInt();
+     System.out.println("interest="+interest);
+     
+     String savingmethod = w.nextLine();
+     System.out.println("savingmethod="+savingmethod);
+     
+}
+
+}
